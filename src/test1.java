10746a546d572b7d919ad349e047eb3cdc29e017
zeynepmerdogan <zeynepmelekerdogan@gmail.com>
2020-11-25 20:32:06 +0100

    Yeni Sepete ekle kismi yazildi

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Ürünleri listeleme yazildi");
-        System.out.println("Sepete ekle kismi yazildi");
+        System.out.println("Yeni Sepete ekle kismi yazildi");
+        System.out.println("Yeni Sepete ekle 2.kismi yazildi");
     }
 }
